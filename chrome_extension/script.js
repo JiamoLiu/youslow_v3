53dd87dbd9f0e83dbc59b238cf363bd4117ee5a4
Khalid Mihlar <khalid.mihlar@gmail.com>
2023-09-18 16:38:04 -0700

    General Changes

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -10,9 +10,37 @@ const report_time = 250;
 
 function postReport(url, jsonData) {
     // this function sends json data to report server
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("POST", url, true);
+    // xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    // xhr.send(JSON.stringify(jsonData));
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+    // Define a callback for when the request completes successfully
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            // Successful response
+            if (successCallback && typeof successCallback === "function") {
+                successCallback(xhr.responseText);
+            }
+        } 
+        else {
+        // Error response
+            if (errorCallback && typeof errorCallback === "function") {
+                errorCallback(xhr.status, xhr.statusText);
+            }
+        }
+    };
+    // Handle network errors
+    xhr.onerror = function () {
+        if (errorCallback && typeof errorCallback === "function") {
+            errorCallback(xhr.status, "Network Error");
+        }
+    };
+
+    // Send the JSON data as the request body
     xhr.send(JSON.stringify(jsonData));
 }
 
