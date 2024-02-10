d1b923f367597ac1bbb0370ac0ee92a750a06b5a
Khalid Mihlar <khalid.mihlar@gmail.com>
2024-02-09 18:02:32 -0800

    Finished working QoE and QoS data collection for TikTok

--- browser_scripts/tiktok_analyze.js
+++ browser_scripts/tiktok_analyze.js
@@ -1,19 +1,95 @@
 var player = null
 var currentURL = null
+var interval = 1000
 var sendData = {}
 var extension_loaded_time = Date.now()
+let intervalID = null;
+let safeProperties = ['ATTRIBUTE_NODE','CDATA_SECTION_NODE','COMMENT_NODE','DOCUMENT_FRAGMENT_NODE','DOCUMENT_NODE','DOCUMENT_POSITION_CONTAINED_BY','DOCUMENT_POSITION_CONTAINS','DOCUMENT_POSITION_DISCONNECTED','DOCUMENT_POSITION_FOLLOWING','DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC','DOCUMENT_POSITION_PRECEDING','DOCUMENT_TYPE_NODE','ELEMENT_NODE','ENTITY_NODE','ENTITY_REFERENCE_NODE','HAVE_CURRENT_DATA','HAVE_ENOUGH_DATA','HAVE_FUTURE_DATA','HAVE_METADATA','HAVE_NOTHING','NETWORK_EMPTY','NETWORK_IDLE','NETWORK_LOADING','NETWORK_NO_SOURCE','NOTATION_NODE','PROCESSING_INSTRUCTION_NODE','TEXT_NODE','accessKey','attributeStyleMap','attributes','autocapitalize','autofocus','autoplay','baseURI','buffered','childElementCount','childNodes','children','classList','className','clientHeight','clientLeft','clientTop','clientWidth','contentEditable','controls','controlsList','crossOrigin','currentSrc','currentTime','dataset','defaultMuted','defaultPlaybackRate','dir','disablePictureInPicture','disableRemotePlayback','draggable','duration','elementTiming','ended','enterKeyHint','height','hidden','id','inert','innerHTML','innerText','inputMode','isConnected','isContentEditable','lang','localName','loop','muted','namespaceURI','networkState','nonce','offsetHeight','offsetLeft','offsetParent','offsetTop','offsetWidth','outerHTML','outerText','ownerDocument','parentElement','parentNode','part','paused','playbackRate','played','playsInline','poster','preload','preservesPitch','readyState','remote','scrollHeight','scrollLeft','scrollTop','scrollWidth','seekable','seeking','sinkId','slot','spellcheck','src','style','tabIndex','tagName','textContent','textTracks','title','translate','videoHeight','videoWidth','virtualKeyboardPolicy','volume','webkitAudioDecodedByteCount','webkitDecodedFrameCount','webkitDisplayingFullscreen','webkitDroppedFrameCount','webkitSupportsFullscreen','webkitVideoDecodedByteCount']
 
-while (!document.getElementsByTagName("video")) {
-    (async () => {
-        await new Promise(r => setTimeout(r, 100));
-    })();
-}
-
-const intervalID = setInterval(function () {
-    const data = document.getElementsByTagName("video");
-    if (data.length > 0){
-        currentURL = window.location.href;
-        player = document.getElementsByTagName("video");
-        console.log(player)
+// Function to update sendData
+function updateSendData() {
+    const video = document.getElementsByTagName("video");
+    if (video) {
+        sendData.ATTRIBUTE_NODE = video[0].ATTRIBUTE_NODE
+        sendData.CDATA_SECTION_NODE = video[0].CDATA_SECTION_NODE
+        sendData.COMMENT_NODE = video[0].COMMENT_NODE
+        sendData.DOCUMENT_FRAGMENT_NODE = video[0].DOCUMENT_FRAGMENT_NODE
+        sendData.DOCUMENT_NODE = video[0].DOCUMENT_NODE
+        sendData.DOCUMENT_POSITION_CONTAINED_BY = video[0].DOCUMENT_POSITION_CONTAINED_BY
+        sendData.DOCUMENT_POSITION_CONTAINS = video[0].DOCUMENT_POSITION_CONTAINS
+        sendData.DOCUMENT_POSITION_DISCONNECTED = video[0].DOCUMENT_POSITION_DISCONNECTED
+        sendData.DOCUMENT_POSITION_FOLLOWING = video[0].DOCUMENT_POSITION_FOLLOWING
+        sendData.DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC = video[0].DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC
+        sendData.DOCUMENT_POSITION_PRECEDING = video[0].DOCUMENT_POSITION_PRECEDING;
+        sendData.DOCUMENT_TYPE_NODE = video[0].DOCUMENT_TYPE_NODE;
+        sendData.ELEMENT_NODE = video[0].ELEMENT_NODE;
+        sendData.ENTITY_NODE = video[0].ENTITY_NODE;
+        sendData.ENTITY_REFERENCE_NODE = video[0].ENTITY_REFERENCE_NODE;
+        sendData.HAVE_CURRENT_DATA = video[0].HAVE_CURRENT_DATA;
+        sendData.HAVE_ENOUGH_DATA = video[0].HAVE_ENOUGH_DATA;
+        sendData.HAVE_FUTURE_DATA = video[0].HAVE_FUTURE_DATA;
+        sendData.HAVE_METADATA = video[0].HAVE_METADATA;
+        sendData.HAVE_NOTHING = video[0].HAVE_NOTHING;
+        sendData.NETWORK_EMPTY = video[0].NETWORK_EMPTY;
+        sendData.NETWORK_IDLE = video[0].NETWORK_IDLE;
+        sendData.NETWORK_LOADING = video[0].NETWORK_LOADING;
+        sendData.NETWORK_NO_SOURCE = video[0].NETWORK_NO_SOURCE;
+        sendData.NOTATION_NODE = video[0].NOTATION_NODE;
+        sendData.PROCESSING_INSTRUCTION_NODE = video[0].PROCESSING_INSTRUCTION_NODE;
+        sendData.TEXT_NODE = video[0].TEXT_NODE;
+        sendData.attributes = video[0].attributes;
+        sendData.autocapitalize = video[0].autocapitalize;
+        sendData.autofocus = video[0].autofocus;
+        sendData.autoplay = video[0].autoplay;
+        sendData.baseURI = video[0].baseURI;
+        sendData.buffered = video[0].buffered;
+        sendData.contentEditable = video[0].contentEditable;
+        sendData.controls = video[0].controls;
+        sendData.controlsList = video[0].controlsList;
+        sendData.crossOrigin = video[0].crossOrigin;
+        sendData.currentSrc = video[0].currentSrc;
+        sendData.currentTime = video[0].currentTime;
+        sendData.defaultMuted = video[0].defaultMuted;
+        sendData.defaultPlaybackRate = video[0].defaultPlaybackRate;
+        sendData.disablePictureInPicture = video[0].disablePictureInPicture;
+        sendData.disableRemotePlayback = video[0].disableRemotePlayback;
+        sendData.draggable = video[0].draggable;
+        sendData.duration = video[0].duration;
+        sendData.ended = video[0].ended;
+        sendData.extension_loaded_time = extension_loaded_time
+        sendData.isConnected = video[0].isConnected;
+        sendData.isContentEditable = video[0].isContentEditable;
+        sendData.localName = video[0].localName;
+        sendData.loop = video[0].loop;
+        sendData.muted = video[0].muted;
+        sendData.namespaceURI = video[0].namespaceURI;
+        sendData.networkState = video[0].networkState;
+        sendData.outerHTML = video[0].outerHTML;
+        sendData.outerText = video[0].outerText;
+        sendData.ownerDocument = video[0].ownerDocument;
+        sendData.parentElement = video[0].parentElement;
+        sendData.parentNode = video[0].parentNode;
+        sendData.paused = video[0].paused;
+        sendData.platform = "tiktok"
+        sendData.playbackRate = video[0].playbackRate;
+        sendData.played = video[0].played;
+        sendData.playsInline = video[0].playsInline;
+        sendData.preload = video[0].preload;
+        sendData.preservesPitch = video[0].preservesPitch;
+        sendData.readyState = video[0].readyState;
+        sendData.remote = video[0].remote;
+        sendData.src = video[0].src;
+        sendData.translate = video[0].translate;
+        sendData.url = window.location.href
+        sendData.videoHeight = video[0].videoHeight;
+        sendData.videoWidth = video[0].videoWidth
+        sendData.webkitAudioDecodedByteCount = video[0].webkitAudioDecodedByteCount
+        sendData.webkitDecodedFrameCount = video[0].webkitDecodedFrameCount
+        sendData.webkitDisplayingFullscreen = video[0].webkitDisplayingFullscreen
+        sendData.webkitDroppedFrameCount = video[0].webkitDroppedFrameCount
+        sendData.webkitSupportsFullscreen = video[0].webkitSupportsFullscreen
+        sendData.webkitVideoDecodedByteCount = video[0].webkitVideoDecodedByteCount
     }
-}, 250);
+}
+updateSendData();
+return sendData;
--- tokgrab.py
+++ tokgrab.py
@@ -1,33 +1,99 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 import time 
+import re
+import os
+import pandas as pd
+import json
 
-tiktok_urls = ["https://www.tiktok.com/@hey_im_moodie/video/7294260320426839342",
-        "https://www.tiktok.com/@hey_im_moodie/video/7259463538891066667",
-        "https://www.tiktok.com/@cg5beats/video/7288033141762133291",
-        "https://www.tiktok.com/@gamexplain/video/7070879107264875818",
-        "https://www.tiktok.com/@ucsbvsa/video/7301555633088777518",
-        "https://www.tiktok.com/@ucsbvsa/video/7297420252030225706",
-        "https://www.tiktok.com/@avantgardey_/video/7288298668883594497",
-        "https://www.tiktok.com/@laoceats/video/7301496364578508078",
-        "https://www.tiktok.com/@oliviaroseolson/video/6998945191709117701",
-        "https://www.tiktok.com/@avantgardey_/video/7260404785260563720",
-        ]
+tiktok_urls =  ["https://www.tiktok.com/@justmaiko/video/6841591262680730886",
+"https://www.tiktok.com/@justmaiko/video/7319932559876525342",
+"https://www.tiktok.com/@bbykevv_/video/7298031771952729387",
+"https://www.tiktok.com/@richieejuice/video/7299625993742896415",
+"https://www.tiktok.com/@soobinismyboyfriend/video/7326419745123749162",
+"https://www.tiktok.com/@nevaaadaa/video/7316681825387171077",
+"https://www.tiktok.com/@taina/video/7297339538685381894",
+"https://www.tiktok.com/@theanimemen/video/7323433434108464430",
+"https://www.tiktok.com/@theanimemen/video/7237339371135880494",
+"https://www.tiktok.com/@theanimemen/video/7232372653946850603"]
+script_path = "./browser_scripts/tiktok_analyze.js"
+chromeTest = True
+data_path = "./stats_data"
+report_time = 0.25
+experiment = 0
+bandwidth = 0
+length = 15
 
+def read_script(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+    
+def wait_for_element(driver, css_selector, timeout=45):
+    end_time = time.time() + timeout
+    print(f"Waiting for element with selector {css_selector}...")
+    while True:
+        try:
+            element = driver.find_elements(By.CSS_SELECTOR, css_selector)
+            if element:
+                return element[0]
+            elif time.time() > end_time:
+                raise TimeoutError(f"Element with selector {css_selector} not found in {timeout} seconds")
+        except NoSuchElementException:
+            pass
+        time.sleep(0.05)  # Wait 50ms before trying again
+
+def record_xhr_requests(driver, proto, pcap_filename):
+    logs_raw = driver.get_log("performance")
+    logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+    with open(f'pcaplogs/{pcap_filename}_{proto}.json', 'w') as outfile:
+        json.dump(logs, outfile)
+
+js_analyze = read_script(script_path)
+
+def append_to_csv(data, num):
+    video_id = re.search(r'(\d+)$', data['url']).group()
+    csv_file = f"{data_path}/vid_{num}_{video_id}.csv"
+    df = pd.DataFrame(columns=data.keys())
+    df = df._append([df, pd.DataFrame([data])], ignore_index=True)
+    if os.path.exists(csv_file):
+        df.to_csv(csv_file, index=False,mode="a",header=False)
+    else:
+        df.to_csv(csv_file, index=False,mode="w",header=True)
+    return video_id
 
 if __name__ == "__main__":
-    chrome = False
-    script_path = "./browser_scripts/tiktok_analyze"
 
-    if chrome:
+    if chromeTest:
+
         s = Service(r"/usr/local/bin/chromedriver")
+
         chromium_executable_path = '/usr/bin/google-chrome'
         chrome_options = Options()
+        chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+        chrome_options.add_experimental_option('perfLoggingPrefs', {'enableNetwork': True})
         # chrome_options.add_argument("--headless")  # Runs Chrome without UI
         chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
         chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-        chrome_options.add_extension("TokSlow.crx")
+
 
         driver = webdriver.Chrome(service=s, options=chrome_options)
+        print("Loading page...")
+        counter = 0
+        print("Currently Processing Video:", counter)
+        for url in tiktok_urls:
+            driver.get(url)
+            while(True):
+                ended=False
+                send_data = driver.execute_script(js_analyze)
+                video_id = append_to_csv(send_data, counter)
+                time.sleep(report_time)
+                ended = driver.execute_script("return document.getElementsByTagName('video')[0].ended")                    
+                if(ended):
+                    record_xhr_requests(driver, video_id, counter)
+                    break
+                
+            time.sleep(1)
+            counter+=1
